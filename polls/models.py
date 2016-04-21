9eed87d2fe52b1cff855845062aadb48b6acb98b
Woo <abyungeun@gmail.com>
2016-04-21 22:19:17 +0900

    models 관계 추가

--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,24 @@
-from django.db import models
+import datetime
 
+from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField('date published') #pub_date컬럼에 대한 레이블 문고입니다 admin사이트에서 이문구 확인 가능함
 
 
 class Choice(models.Model):
+    def __str__(self):
+        return self.choice_text
+
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
